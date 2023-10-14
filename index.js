20a65551cefc95d965f0284233652f9777fc9569
Edwin Okoronkwo <49468467+EdwinOkoronkwo@users.noreply.github.com>
2023-10-14 12:20:27 -0600

    added body type

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 function calculate() {
+  let result;
   const x = parseFloat(document.forms["myForm"]["fweight"].value);
   const y = parseFloat(document.forms["myForm"]["fheight"].value);
-  const bmi = x / y;
+  const bmi = x / (y ^ 2);
+  if (bmi <= 18.5) result = "Under weight";
+  else if (bmi <= 24.9) result = "Normal";
+  else if (bmi <= 29.9) result = "Overweight";
+  else result = "Obese";
   document.getElementById("p1").innerHTML = `${bmi.toFixed(2)} BMI`;
+  document.getElementById("p2").innerHTML = result;
 }
 
 function reset() {
